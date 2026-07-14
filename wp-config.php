6516e08cd8f37ea462d73e955618f9f4aec50a8b
kakadacancer-code <your@email.com>
2026-07-14 14:48:38 +0700

    .

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'vbanx_project' );
+define( 'DB_NAME', 'vbanx_db' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
